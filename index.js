74a29cc9755cf49c0c2e708c7c9f9f8c782fdd2f
himanshu09193 <himanshu9193@gmail.com>
2021-06-24 10:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello your node js app on openshift is up and running!\n'
   res.end(msg);
 });
 
